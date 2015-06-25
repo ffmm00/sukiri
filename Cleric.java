960bb0bdb8aab7d2c4f13584cd96312fcf6f2ee9
ffmm <masamail4210@gmail.com>
2015-06-25 16:14:40 +0900

    9-10章

--- Cleric.java
+++ Cleric.java
@@ -1,5 +1,3 @@
-import java.util.Random;
-
 public class Cleric {
 	static final int MAX_HP = 50;
 	static final int MAX_MP = 10;
@@ -20,19 +18,7 @@ public class Cleric {
 	}
 
 	public Cleric(String name) {
-		this(name, MAX_HP);
+		this(name, Cleric.MAX_HP);
 	}
 
-	public void selfAid() {
-		this.mp -= 5;
-		this.hp = this.MAX_HP;
-	}
-
-	public int pray(int sec) {
-		int r = sec + new Random().nextInt(3);
-		int R = Math.min(r, this.MAX_MP - this.mp);
-
-		this.mp += R;
-		return R;
-	}
 }
